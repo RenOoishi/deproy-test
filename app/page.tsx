a2d756ea7f7a4ed2a150883b884717329502f68e
RenOoishi <o-ren-2409@occ.co.jp>
2024-05-02 10:30:27 +0900

    クリックできるように変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="w-screen">
       <div className="h-screen flex flex-col justify-center items-center bg-black">
-        <h1 className="font-bold text-lg text-neutral-100">Hello Page</h1>
+        <Link href={"https://www.google.com/search?q=cat&sca_esv=782705a13c1e22f4&sca_upv=1&udm=2&biw=1536&bih=703&ei=XuwyZtHNHKzk2roP9OSnqA4&ved=0ahUKEwjR9NG56O2FAxUsslYBHXTyCeUQ4dUDCBA&oq=cat&gs_lp=Egxnd3Mtd2l6LXNlcnAiA2NhdDIKEAAYgAQYsQMYBDIHEAAYgAQYBDIHEAAYgAQYBDIKEAAYgAQYsQMYBDIHEAAYgAQYBDIQEAAYgAQYsQMYgwEYBBiKBTIKEAAYgAQYsQMYBDIGEAAYAxgEMgoQABiABBixAxgEMgoQABiABBixAxgESOsQUABYAHABeACQAQCYAQCgAQCqAQC4AQzIAQCYAgGgAgWYAwCIBgGSBwExoAcA&sclient=gws-wiz-serp"}
+         className="font-bold text-lg text-neutral-100">
+          Hello Page
+        </Link>
       </div>
     </div>
   );
